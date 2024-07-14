842778b38560f0fb489b3f81dff65186176ed5b6
Aidan Melvin <aidmelvin.314@gmail.com>
2024-07-14 08:58:09 -0700

    updated code

--- script.js
+++ script.js
@@ -19,7 +19,5 @@ console.log(greet("Student"));
 
 // Document Object Model (DOM)
 // Manipulating HTML elements with document.getElementById
-document.addEventListener("DOMContentLoaded", function() {
-    let heading = document.getElementById("uniqueElement");
-    heading.textContent = "This is a modified heading!";
-});
+let heading = document.getElementById("uniqueElement").innerText = "Modified from Javascript"
+
